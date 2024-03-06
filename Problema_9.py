f764458028e87a92935e7e71f049a14342325d46
Paul <129361725+pvilcapazab@users.noreply.github.com>
2024-03-06 19:59:19 +0000

    Solución al Problema_9

---
+++ Problema_9.py
@@ -0,0 +1,7 @@
+#Guardar la lista en una variable e imprimir la lista para verla
+lista = ['di', 'buen', 'día', 'a', 'papá']
+print(f"Lista original: {lista}")
+
+#Invertir la lista original e imprimir la lista para verla
+lista.reverse()
+print(f"Lista invertida: {lista}")
